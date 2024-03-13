d8d2f90df7ba74ade30ddcd9843f04baaddeb4b8
amelie314 <qangela314@gmail.com>
2024-03-13 21:00:34 +0800

    🔍 feat: 找出不一樣的資料

---
+++ 0036/0036.js
@@ -0,0 +1,32 @@
+/** @format */
+
+// 36. 找出不一樣的資料
+// 輸入一個至少有三筆資料的整數陣列 / 列表，其中包含唯一一個和其他所有資料不同的整數。你的函式能找出並回傳這個整數所在的索引位置。
+
+// 輸入範例：[5, 5, 0]
+// 回傳：2
+
+// 輸入範例：[1, 2, 1, 1, 1]
+// 回傳：1
+
+// 輸入範例：[-5, 8, 8, 8]
+// 回傳：0
+
+/*
+    @param ns:{[Integer]}
+    @return :{Integer}
+*/
+function findUnique(ns) {
+  for (let i = 0; i < ns.length; i++) {
+    let ifNuique = true;
+    for (let j = 0; j < ns.length; j++) {
+      if (i !== j && ns[i] === ns[j]) {
+        ifNuique = false;
+        break;
+      }
+    }
+    if (ifNuique) {
+      return i;
+    }
+  }
+}
